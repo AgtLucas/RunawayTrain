7631f1a283162e5f0f0f0f2f0c3f98b9459bcd93
Lucas <lucas@agtlucas.com>
2016-04-21 10:54:16 -0300

    Update styles

--- index.ios.js
+++ index.ios.js
@@ -33,6 +33,9 @@ class RunawayTrain extends Component {
       .then(response => response.json())
       .then(jsonData => {
         console.log(jsonData)
+        this.setState({
+          isLoading: false
+        })
       })
       .catch(error => console.log(`Fetch error: ${error}`))
   }
@@ -45,7 +48,7 @@ class RunawayTrain extends Component {
           color={'#FFF'}
           size={'small'}
           style={{margin: 15}} />
-        <Text style={{color: '#fff'}}>Unsplash &copy;</Text>
+        <Text style={{color: '#fff'}}>Contacting Unsplash api</Text>
       </View>
     )
   }
@@ -66,40 +69,16 @@ class RunawayTrain extends Component {
     } else {
       return this.renderResults()
     }
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  loadingContainer: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+    backgroundColor: '#000'
   }
 })
 
