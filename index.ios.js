1f5649605543146176359c33df829deeff25da81
Lucas <lucas@agtlucas.com>
2016-04-21 00:10:40 -0300

    JS Standard

--- index.ios.js
+++ index.ios.js
@@ -10,10 +10,10 @@ import React, {
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
 class RunawayTrain extends Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -27,7 +27,7 @@ class RunawayTrain extends Component {
           Cmd+D or shake for dev menu
         </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -36,18 +36,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
 
-AppRegistry.registerComponent('RunawayTrain', () => RunawayTrain);
+AppRegistry.registerComponent('RunawayTrain', () => RunawayTrain)
